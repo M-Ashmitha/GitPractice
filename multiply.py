607a60e2748ca298ebaf71d216070b973ebeab4d
M-Ashmitha <68429976+M-Ashmitha@users.noreply.github.com>
2022-12-13 14:04:48 +0530

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -2,5 +2,5 @@ a = 10
 b = 20
 
 print(a*b)
-print("File added from local repository)
-print("Change made in repository)
+print("File added from local repository")
+print("Change made in repository")
